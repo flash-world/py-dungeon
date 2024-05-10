efc837fe812fc863708efebe1cf478fc084a91f7
flash-world <160246400+flash-world@users.noreply.github.com>
2024-05-10 12:50:35 -0400

    yes

---
+++ main.py
@@ -0,0 +1,6 @@
+print("         ----------------------          ")
+print("         Welcome to py-dungeon!          ")
+print("         ----------------------          ")
+print("                                         ")
+print("(hit enter to continue)")
+KeyboardInterrupt
