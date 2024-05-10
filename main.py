f21f083df0f873deac9386722ee3df3ec276016f
flash-world <160246400+flash-world@users.noreply.github.com>
2024-05-10 14:14:22 -0400

    ask for name

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import keyboard
 print("         ----------------------          ")
 print("         Welcome to py-dungeon!          ")
 print("         ----------------------          ")
 print("                                         ")
-print("(hit enter to continue)")
-KeyboardInterrupt
+print("Hit any key to continue")
+keyboard.wait()
+print("What will you be known as?")
+yn = false
+while yn == False:
+    name= input("Name:")
+    print("     ")
+    ans=""
+    while ans !="y" or ans !="n":
+        print("Do you want to be known as "+name+"? (y/n)")
+        if ans=="y":
+            yn == True
+            break
+    if yn == True:
+        break
+
